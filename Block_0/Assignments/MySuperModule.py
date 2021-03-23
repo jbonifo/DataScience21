044a292d2d95cbc794cdc7c659f89353e4a555e4
jbonifo <bonifondjoshua@gmail.com>
2021-03-23 19:38:43 +0100

    def add

--- Block_0/Assignments/MySuperModule.py
+++ Block_0/Assignments/MySuperModule.py
@@ -1,1 +1,11 @@
-Class ListKeeper : 
+class ListKeeper : 
+    def _init_(self):
+        self.Keeper = {'example': [1,2,3,4,5]}
+        
+    def show(self):
+        for key in self.Keeper
+            print(key)
+            
+    def add(self,name,list):
+        
+    
